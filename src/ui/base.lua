dbe35018dd2df9fb9aae17ad0be2da4a3f9aae7e
Osvald Lindholm <osvald.lindholm@gmail.com>
2023-06-19 19:11:24 +0200

    Initial commit v. 1.0.0

---
+++ src/ui/base.lua
@@ -0,0 +1,66 @@
+--
+-- Base
+-- @Auther Olindholm
+--
+
+if not Base then
+    Base = {}
+    -- Need an interface to register watchers to.
+    -- 'main' is the only one that works 100 % of the time
+    -- TODO replace with UIManager.GetActiveInterface() ?
+    local interface = UIManager.GetInterface('main')
+
+    function GetSide()
+        if GetCvarBool('ui_minimap_rightside') then
+            return 'right'
+        else
+            return 'left'
+        end
+    end
+
+    function GetOppositeSide()
+        if not GetCvarBool('ui_minimap_rightside') then
+            return 'right'
+        else
+            return 'left'
+        end
+    end
+
+    function StripClanTag(playerName)
+        if (string.find(playerName, ']')) then
+            playerName = string.sub(playerName, string.find(playerName, ']') + 1)
+        end
+        return playerName
+    end
+
+    function IsClientVoiceMuted(client)
+        return AtoB(UIManager.GetActiveInterface():UICmd("IsVoiceMuted('" .. client .. "')"))
+    end
+
+    function IsPlayerVoiceMuted(playerName)
+        local client = GetClientByPlayerName(playerName)
+        if (client == nil) then return false end
+
+        return IsClientVoiceMuted(client)
+    end
+
+    local MaxAllies = 4
+    local ClientByPlayerName = {}
+
+    --
+    -- Returns the client number from a player's name.
+    -- If no player with the provided name is not connected, nil is returned.
+    function GetClientByPlayerName(playerName)
+        local striptPlayerName = StripClanTag(playerName)
+        return ClientByPlayerName[playerName]
+    end
+
+    local function AllyPlayerInfo(allyIndex, sourceWidget, playerName, playerColor, playerClient)
+        local striptPlayerName = StripClanTag(playerName)
+        ClientByPlayerName[playerName] = playerClient
+    end
+
+    for i = 0, MaxAllies - 1 do
+        interface:RegisterWatch('AllyPlayerInfo' .. i, function(...) AllyPlayerInfo(i, ...) end)
+    end
+end
---
+++ src/ui/scripts/newui/fav_heroes.lua
@@ -0,0 +1,208 @@
+--
+-- Fav Heroes
+-- @Auther Olindholm
+--
+require('/ui/base.lua')
+
+local interface = object
+local interfaceName = interface:GetName()
+
+local favHeroesCount = 5
+local maxLobbyPlayers = 10
+local maxFetchAttempts = 5
+
+local playerNames = {}
+local fetchAttempts = {}
+
+local function GetHeroIconPath(heroName)
+    if Empty(heroName) then
+        return '/ui/common/ability_coverup.tga'
+    end
+
+    return '/heroes/' .. heroName .. '/icon.tga'
+end
+
+
+local function GetLobbyPlayerFavHeroIconWidget(playerIndex, heroIndex)
+    return interface:GetWidget(
+        'lobby_player_' .. playerIndex .. '_fav_hero_' .. (heroIndex - 1) .. '_icon'
+    )
+end
+local function GetLobbyPlayerFavHeroPercetangeWidget(playerIndex, heroIndex)
+    return interface:GetWidget(
+        'lobby_player_' .. playerIndex .. '_fav_hero_' .. (heroIndex - 1) .. '_percentage'
+    )
+end
+local function GetLobbyPlayerFavHeroesLoadingWidget(playerIndex)
+    return interface:GetWidget('lobby_player_' .. playerIndex .. '_fav_hero_loading')
+end
+local function GetLobbyPlayerFavHeroesSleeperWidget(playerIndex)
+    return interface:GetWidget('LobbyPlayer' .. playerIndex .. 'FavHeroesSleeper')
+end
+
+local function ParseFavHeroes(params)
+    local favHeroes = {}
+
+    for i = 1, favHeroesCount do
+        local favHero      = {}
+        favHero.name       = params[i]
+        favHero.percentage = params[i + favHeroesCount]
+        favHero.name2      = params[i + favHeroesCount * 2]
+        favHeroes[i]       = favHero
+    end
+
+    return favHeroes
+end
+
+local function ResetLobbyPlayerFavHeroes(playerIndex)
+    for i = 1, favHeroesCount do
+        GetLobbyPlayerFavHeroIconWidget(playerIndex, i):SetTexture('$invis')
+        GetLobbyPlayerFavHeroPercetangeWidget(playerIndex, i):SetText('')
+    end
+end
+
+local function FetchLobbyPlayerFavHeroes(index, playerName)
+    local attempt = 1
+
+    if Empty(playerName) then
+        if Empty(playerNames[index]) then
+            return
+        else
+            playerName = playerNames[index]
+            attempt = fetchAttempts[index] + 1
+
+            if attempt > maxFetchAttempts then
+                return
+            end
+        end
+    end
+
+    println(
+        'Fetching favorites heroes for ' .. playerName ..
+        ' (' .. index .. ') (' .. attempt .. '/' .. maxFetchAttempts .. ')'
+    )
+    SubmitForm(
+        'LobbyPlayer' .. index .. 'FavHeroes',
+        'f',
+        'show_stats',
+        'nickname',
+        StripClanTag(playerName),
+        'cookie',
+        Client.GetCookie(),
+        'table',
+        'campaign'
+    )
+    playerNames[index] = playerName
+    fetchAttempts[index] = attempt
+end
+
+
+local function OnLobbyPlayerInfo(index, clientNum, playerName)
+    if clientNum == '-1' then
+        return
+    end
+
+    ResetLobbyPlayerFavHeroes(index)
+    FetchLobbyPlayerFavHeroes(index, playerName)
+end
+
+local function OnLobbyPlayerFavHeroesStatus(index, status)
+    status = tonumber(status)
+    local loading = (status == 1)
+
+    GetLobbyPlayerFavHeroesLoadingWidget(index):SetVisible(loading)
+
+    if loading then
+        return -- Loading... / Pending...
+    end
+
+    local playerName = playerNames[index]
+
+    if status == 2 then
+        --println('Finished retrival of fav heroes for ' .. playerName .. ' (' .. index .. ')')
+    elseif status == 3 then
+        --println('Failed retrival of fav heroes for ' .. playerName .. ' (' .. index .. ')')
+        GetLobbyPlayerFavHeroesSleeperWidget(index):Sleep(
+            0,
+            function(...) FetchLobbyPlayerFavHeroes(index, nil) end
+        )
+    end
+end
+
+local function OnLobbyPlayerFavHeroesResult(index, error, favHeroes)
+    for i = 1, favHeroesCount do
+        local favHero = favHeroes[i]
+        local percentage = math.floor(favHero.percentage + 0.5)
+
+        GetLobbyPlayerFavHeroIconWidget(index, i):SetTexture(GetHeroIconPath(favHero.name))
+        GetLobbyPlayerFavHeroPercetangeWidget(index, i):SetText(tostring(percentage) .. ' %')
+
+        println(
+            'LobbyPlayer: ' .. index .. ': ' ..
+            string.format('%-15s', favHero.name) .. ' => ' ..
+            string.format('%2d', percentage) .. ' %'
+        )
+    end
+end
+
+for i = 0, maxLobbyPlayers - 1 do
+    interface:RegisterWatch(
+        'LobbyPlayerInfo' .. i,
+        function(_, ...) OnLobbyPlayerInfo(i, ...) end
+    )
+    interface:RegisterWatch(
+        'LobbyPlayer' .. i .. 'FavHeroesStatus',
+        function(_, ...) OnLobbyPlayerFavHeroesStatus(i, ...) end
+    )
+    interface:RegisterWatch(
+        'LobbyPlayer' .. i .. 'FavHeroesResult',
+        function(_, error, ...) OnLobbyPlayerFavHeroesResult(i, error, ParseFavHeroes(arg)) end
+    )
+end
+
+function Dump(o)
+    local metatable = getmetatable(o)
+
+    local keys = {}
+    for key in pairs(metatable) do
+        table.insert(keys, key)
+    end
+
+    table.sort(keys)
+
+    for _, key in ipairs(keys) do
+        local value = metatable[key]
+        println(tostring(key) .. ' => ' .. tostring(value));
+    end
+end
+
+local function OnTestFavHeroes(...)
+    local testPlayerNames = {
+        'unknownNOOB',
+        '[TITB]`LUFFY`',
+        'Feol',
+        'Hokter',
+        'Trinity',
+        'MILF_',
+        'Freak',
+        'Nakano',
+        'SideStepGod',
+        'Cyou',
+        'Hexe',
+        'DaftPunk'
+    }
+
+    for i = 0, math.min(maxLobbyPlayers, #testPlayerNames) - 1 do
+        local playerName = testPlayerNames[i + 1]
+        OnLobbyPlayerInfo(i, i, playerName)
+    end
+end
+
+interface:RegisterWatch('TestFavHeroes', function(_, ...) OnTestFavHeroes(...) end)
+
+-- This must be placed at the end to
+-- be able to "use" all functions.
+-- It must also be global (aka not local)
+FavHeroes = {}
+function FavHeroes:Init()
+end
